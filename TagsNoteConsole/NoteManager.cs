d2a9da3b5182817a8ea8a5808bb848a0b11d6685
IvanZ <sabotage84@mail.ru>
2018-07-06 21:00:11 +0300

    Add manager

---
+++ TagsNoteConsole/NoteManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsNoteConsole
+{
+    public class NoteManager
+    {
+        public List<Note> notes;
+
+        public NoteManager()
+        {
+            notes = new List<Note>();
+        }
+
+        public NoteManager(List<Note> _notes)
+        {
+            notes = _notes;
+        }
+
+        public void AddNote(Note newNote)
+        {
+            notes.Add(newNote);
+        }
+
+        public void AddNote(string title, List<string> tags)
+        {
+            notes.Add(new Note(title, tags));
+        }
+
+        public void DeleteNote(Note note)
+        {
+            notes.Remove(note);
+        }
+
+    }
+}
---
+++ UnitTestProject2/NoteManagerTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TagsNoteConsole;
+
+namespace UnitTestProject2
+{
+    /// <summary>
+    /// Сводное описание для NoteManagerTest
+    /// </summary>
+    [TestClass]
+    public class NoteManagerTest
+    {
+
+        [TestMethod]
+        public void TestAddNote()
+        {
+            NoteManager testManager = new NoteManager();
+            Note note = new Note("testAdd", new List<string> { "tag33", "TAG44" });
+            testManager.AddNote(note);
+            Assert.IsTrue(testManager.notes.Contains(note));
+        }
+
+        [TestMethod]
+        public void TestAddNote2()
+        {
+            NoteManager testManager = new NoteManager();
+            
+            testManager.AddNote("testAdd", new List<string> { "tag33", "TAG44" });
+
+            Note note = new Note("testAdd", new List<string> { "tag33", "TAG44"});
+            Assert.IsTrue(testManager.notes.Count==1);
+        }
+
+        [TestMethod]
+        public void TestDeleteNote()
+        {
+            NoteManager testManager = new NoteManager();
+
+            testManager.AddNote("testAdd", new List<string> { "tag33", "TAG44" });
+
+            Note note = new Note("testAdd", new List<string> { "tag33", "TAG44" });
+
+            testManager.DeleteNote(note);
+            Assert.IsTrue(testManager.notes.Count == 0);
+        }
+    }
+}
